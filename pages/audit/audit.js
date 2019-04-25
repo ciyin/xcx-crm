7f73b5e6a3f4d0c208478815081c7d0a11e17873
emily <liudanxia@testbay.cn>
2019-04-25 18:00:08 +0800

    修改数据绑定

--- pages/audit/audit.js
+++ pages/audit/audit.js
@@ -1,7 +1,44 @@
 Page({
   data:{
-    numbers:[1,2,3,4,5,6,7,8],
-    index:1
+    contract:{
+      title:'合同审批（周杨）[SH2100332]',
+      original_amount:'50000',
+      total_amount:'20000',
+      discount_amount:'0.8',
+      paid_amount:'2000',
+      signed_by_name:'周婧茹',
+      memo:'老学员8折'
+    },
+    student:{
+      balance:'0'
+    },
+    discounts:[
+      {
+        discount_type:'现金优惠',
+        discount_amount:'33'
+      },
+      {
+        discount_type: '优惠券',
+        discount_name:'内部学员冲刺班优惠',
+        discount_amount: '2990'
+      }
+    ],
+    contract_items:[
+      {
+        product:'TOEFL(阅读)',
+        price:'1080',
+        quantity:'4',
+        unpaid_quantity:'2',
+        coupon_name:'一对一1000优惠券'
+      },
+      {
+        product: 'TRFSH0001',
+        price: '23000',
+        quantity: '1',
+        unpaid_quantity: '1'
+      }
+    ],
+    numbers:[0,1,2,3,4,5,6,7,8]
   },
   back: function () {
     wx.switchTab({
--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,4 +1,40 @@
+var audits = [
+  {
+    title:'合同审批（周一行）[SH2100234]',
+    creator:'刘丹霞',
+    current_step:'合同收款'
+  },
+  {
+    title: '合同审批（朱子语）[SH2100244]',
+    creator: '张中伟',
+    current_step: '校长确认'
+  },
+  {
+    title: '合同审批（杨阳）[SH2100224]',
+    creator: '沈阳',
+    current_step: '合同收款'
+  },
+  {
+    title: '合同审批（李雨桐）[SH2100334]',
+    creator: '张中伟',
+    current_step: '校长确认'
+  },
+  {
+    title: '合同审批（王小珏）[SH2100434]',
+    creator: '沈丽',
+    current_step: '合同收款'
+  },
+  {
+    title: '合同审批（王一博）[SH2100434]',
+    creator: '沈丽',
+    current_step: '合同收款'
+  },
+];
+
 Page({
+  data:{
+    audits
+  },
   audit:function(){
     wx.redirectTo({
       url: '/pages/audit/audit',
